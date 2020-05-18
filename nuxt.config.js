e1ffa0f5fab6cd881bf7322f0e797508f4e94716
Oksana Petrusenko <ksupetrusenko@gmail.com>
2020-05-18 12:10:15 +0800

    Add font awesome

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,6 +12,7 @@ module.exports = {
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', type: 'text/css', href: 'styles/bootstrap.min.css' },
+      { rel: 'stylesheet', type: 'text/css', href: 'styles/font-awesome.min.css' },
     ]
   },
   /*
